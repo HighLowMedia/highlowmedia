ee0fa9f6123997239d681a0c3b2016738fb3ae60
admin <henry@Henrys-MacBook-Pro.local>
2024-12-03 12:16:18 -0700

    new landing logo

--- app/ui/logo.tsx
+++ app/ui/logo.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import logo from '/public/high-low-media-256x256.png';
+import logo from '/public/high-low-media-black-2000x2000.png';
 
 export default function Logo() {
   return (
